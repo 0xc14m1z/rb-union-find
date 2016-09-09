c21378080ce2ba33f4eb2f0e6635e6901b783937
Luca Ramundo <luca@ramundo.info>
2016-09-09 18:20:56 +0200

    Update RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require_relative "../lib/union-find/interface.rb"
+
 RSpec.configure do |config|
+  RSpec::Expectations.configuration.on_potential_false_positives = :nothing
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
