079f45543a2c0cafcf3b9394b3937f1cf0bb0b61
Luca Ramundo <luca@ramundo.info>
2016-09-10 11:43:38 +0200

    Add inheritance to path-compressed-weighted-quick-union algorithm

--- lib/union-find/path_compressed_weighted_quick_union.rb
+++ lib/union-find/path_compressed_weighted_quick_union.rb
@@ -1,39 +1,6 @@
 module UnionFind
 
-  class PathCompressedWeightedQuickUnion < Interface
-
-    attr_reader :components
-    attr_reader :weights
-
-    def initialize
-      @components = Hash.new
-      @weights = Hash.new
-    end
-
-    def count
-      @components.keys.size
-    end
-
-    def add(obj)
-      @components[obj] = obj
-      @weights[obj] = 1
-    end
-
-    def connect(a, b)
-      root_a = root(a)
-      root_b = root(b)
-      if @weights[root_a] < @weights[root_b]
-        @components[root_a] = root_b
-        @weights[root_b] += @weights[root_a]
-      else
-        @components[root_b] = root_a
-        @weights[root_a] += @weights[root_b]
-      end
-    end
-
-    def connected?(a, b)
-      root(a) == root(b)
-    end
+  class PathCompressedWeightedQuickUnion < WeightedQuickUnion
 
     private
 
