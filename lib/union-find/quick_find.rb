03886c3f9f308418a6e948868a37077271fa76ff
Luca Ramundo <luca@ramundo.info>
2016-09-10 09:22:26 +0200

    Add quick-find algorithm implementation

--- lib/union-find/quick_find.rb
+++ lib/union-find/quick_find.rb
@@ -2,6 +2,34 @@ module UnionFind
 
   class QuickFind < Interface
 
+    attr_reader :data
+
+    def initialize
+      @data = Hash.new
+    end
+
+    def count
+      @data.keys.size
+    end
+
+    def add(obj)
+      @data[obj] = obj
+    end
+
+    def connect(a, b)
+      check_value = @data[a]
+      for k in @data.keys
+        @data[k] = b if @data[k] == check_value
+      end
+      # @data.each do |key, value|
+      #   @data[key] = b if value == current_a_link
+      # end
+    end
+
+    def connected?(a, b)
+      @data[a] == @data[b]
+    end
+
   end
 
 end
